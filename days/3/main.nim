02e7622e496ad733684615f328174937c5fd7e52
Benjamin Shirley-Quirk <benjamin.shirley-quirk@geoslam.com>
2021-12-03 11:29:22 +0000

    ugly but works

--- days/3/main.nim
+++ days/3/main.nim
@@ -1,6 +1,7 @@
 ## input is a list of binary numbers
 from std/strutils import parseBinInt
 import std/bitops
+import zero_functional
 ## input -> gamma rate, epsilon rate
 ## gamma rate is most common bit at each position
 ## we could count how many ones and then if it's less than half it becomes a zero
@@ -35,8 +36,76 @@ proc q1[N:static int](filename:string):uint =
             raise new ValueError
     return gamma*epsilon
 
+##ok seriouser now
+
+template diagnosis[bitlen:static int](input:seq[uint],criterion:untyped):uint =
+    var input = input
+    for i{.inject.} in 0..<bitlen:
+        input = input --> filter(criterion)
+        if input.len == 1:
+            return input
+proc oxygenGenerator[bitlen:static int](input:seq[uint]):uint =
+    var input = input
+    for i in countdown(bitlen-1,0):
+        var zeros,ones:seq[uint]
+        for x in input:
+            if ((x shr i) and 1)==1:
+                ones.add x
+            else:
+                zeros.add x
+        #var mostcommon,leastcommon:seq[int]
+        if ones.len >= zeros.len:
+            #mostcommon = move(ones)
+            input = move(ones)
+            #leastcommon = move(zeros)
+        else:
+            input = move(zeros)
+            #mostcommon = move(zeros)
+            #leastcommon = move(ones)
+        if input.len == 1:        
+            return input[0]
+    raise new ValueError
+proc c02Scrubber[bitlen:static int](input:seq[uint]):uint =
+    var input = input
+    for i in countdown(bitlen-1,0):
+        if len(input)==1:
+            return input[0]
+        var zeros,ones:seq[uint]
+        for x in input:
+            if ((x shr i) and 1) == 1:
+                ones.add x
+            else:
+                zeros.add x
+        #var mostcommon,leastcommon:seq[int]
+        if ones.len >= zeros.len:
+            #mostcommon = move(ones)
+            input = move(zeros)
+            #leastcommon = move(zeros)
+        else:
+            input = move(ones)
+            #mostcommon = move(zeros)
+            #leastcommon = move(ones)
+    raise new ValueError
+
+        
+proc q2[N:static int](filename:string):uint =
+    ## life support rating
+    ## oxygen generator rating * c02 scrubber rating
+    ## only first bit
+    ## keep bits that match bit criteria
+    ## if there's one number, stop, this is number
+    ## else consider next bit to the right
+    ## oxygen gen: filter on numbers with most common value
+    ## if 01 equally common-> 1
+    let input:seq[uint] = (filename.lines --> map(it.parseBinInt.uint))
+    return input.c02Scrubber[:N] * input.oxygenGenerator[:N]
+
+
+     
 when isMainModule:
     assert q1[5]("test") == 198
     echo q1[12]("input")
+    assert q2[5]("test") == 230
+    echo q2[12]("input")
 
 

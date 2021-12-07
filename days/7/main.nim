d042fd1daee26754917d74a9b2fc7c41e285d21e
Benjamin Shirley-Quirk <benjamin.shirley-quirk@geoslam.com>
2021-12-07 05:36:44 +0000

    messy

---
+++ days/7/main.nim
@@ -0,0 +1,44 @@
+#median
+import std/[math,algorithm,strutils,sequtils]
+var input = "input".open.readLine.split(',').map(parseInt)
+input.sort()
+echo "median is between",input[499]# 499 less, 500 greater, " and ", input[500]#
+echo "and ", input[500] #500 less, 499 greater 
+echo input.len
+let median = 333
+
+##how much petrol
+proc distance(s:seq[int],n:int):int = 
+    for i in s:
+        result.inc abs(i-n)#sum(collect(for i in s:
+
+
+echo distance(input,333)
+
+#proc fuel(distance:int):int = 
+    #1->1
+    #2->1+2
+    #3->1+2+3 triangular numbers = n*(n+1)/2
+    #we minimize sum(abs( (x-u)*(x-u+1)/2))
+
+proc distance2(s:seq[int],n:int):int = 
+    for i in s:
+        result.inc (abs(i-n)*(abs(i-n)+1) div 2)
+
+var max = int.low
+var min = int.high
+for i in input:
+    if i>max:
+        max = i
+    if i<min:
+        min = i
+echo "min:",min,"max:",max
+
+var mindist = int.high
+var min_n = -1
+for n in 0..1935:
+    let d = distance2(input,n)
+    if d < mindist:
+        mindist = d
+        min_n = n
+        echo "mindist:",d,"min_n",n
